6c039e71f28144b72d75cbc0eae0d0e3409953cd
falkTX <falktx@falktx.com>
2024-06-07 20:04:02 +0200

    Add tests
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ tests/websocket.js
@@ -0,0 +1,113 @@
+
+// globals
+let ws;
+let g_metadata = {
+    categories: [],
+    plugins: [],
+};
+let g_state = {
+    bank: 1,
+    banks: {
+        1: {
+            blocks: {
+                "1": { "name": "-", "uri": "-" },
+                "2": { "name": "-", "uri": "-" },
+                "3": { "name": "-", "uri": "-" },
+                "4": { "name": "-", "uri": "-" },
+                "5": { "name": "-", "uri": "-" },
+                "6": { "name": "-", "uri": "-" },
+            }
+        },
+    },
+};
+
+function wsconnect() {
+    ws = new WebSocket("ws://" + (window.location.hostname || "localhost") + ":13371/websocket")
+
+    ws.onclose = function () {
+        // console.log("onclose", ws)
+
+        if (ws === false) {
+            return;
+        }
+
+        // retry in 1s
+        setTimeout(wsconnect, 1000)
+    }
+
+    ws.onerror = function (evt) {
+        console.log("onerror", evt)
+        // set as false to signal error
+        ws = false;
+    }
+
+    ws.onmessage = function (evt) {
+        // console.log("onmessage", evt)
+        const data = JSON.parse(evt.data)
+        // console.log("onmessage", data)
+
+        switch (data.type) {
+        case 'state':
+            g_metadata.categories = data.categories;
+            g_metadata.plugins = data.plugins;
+
+            // websocket server state can be partial, so be careful when importing it
+            if (data.state) {
+                if (data.state.banks !== undefined) {
+                    for (var b in data.state.banks) {
+                        const databank = data.state.banks[b];
+                        if (databank.blocks !== undefined) {
+                            for (var bl in databank.blocks) {
+                                const datablock = databank.blocks[bl];
+                                g_state.banks[b].blocks[bl] = datablock;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // update page to display retrieved content
+            update_metadata(g_metadata)
+            update_state(g_state)
+            break;
+        }
+    }
+
+    ws.onopen = function () {
+        // ws.send("init");
+    }
+}
+
+// utility functions
+// NOTE partial/missing properties are ignored on webserver
+
+// send full state
+function ws_send_state(state) {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify({
+            type: 'state',
+            state: state,
+        }));
+        return true;
+    }
+    return false;
+}
+
+// send blocks
+function ws_send_blocks(blocks) {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+        console.log("sending live blocks", blocks);
+        ws.send(JSON.stringify({
+            type: 'state',
+            state: {
+                "banks": {
+                    [g_state.bank]: {
+                        blocks: blocks
+                    }
+                }
+            }
+        }));
+        return true;
+    }
+    return false;
+}
