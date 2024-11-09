6f8a6d22095221c864f45331c836d22249d876c1
falkTX <falktx@falktx.com>
2024-11-09 09:23:18 +0100

    Add tests for building interface library
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ tests/buildlib/main.cpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2024 Filipe Coelho <falktx@darkglass.com>
+// SPDX-License-Identifier: ISC
+
+#include "connector.hpp"
+
+int main()
+{
+    HostConnector c;
+    return 0;
+}
