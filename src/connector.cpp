ffbb1373494dd5787b59ebb7acc4a1ad9efd409c
Mikael Ruohonen <mikael@darkglass.com>
2026-04-20 15:17:35 +0300

    Revert "more optimization of replaceBlockWhileKeepingCurrentData"
    This reverts commit 571a551b61d7ebf72b897c02aaf386c85caade17.

--- src/connector.cpp
+++ src/connector.cpp
@@ -1759,11 +1759,23 @@ bool HostConnector::replaceBlock(const uint8_t row,
         if (added)
         {
             ++_current.numLoadedPlugins;
-            initBlock(blockdata, plugin, numInputs, numOutputs, numSideInputs, numSideOutputs, blockDataToCopy);
+            initBlock(blockdata, plugin, numInputs, numOutputs, numSideInputs, numSideOutputs);
 
             // copy data from old block
             if (blockDataToCopy != nullptr)
             {
+                const Block& blockcopy(*blockDataToCopy);
+                blockdata.enabled = blockcopy.enabled;
+                blockdata.quickPotSymbol = blockcopy.quickPotSymbol;
+                blockdata.meta.enable = blockcopy.meta.enable;
+                blockdata.meta.quickPotIndex = blockcopy.meta.quickPotIndex;
+                blockdata.meta.numParametersInScenes = blockcopy.meta.numParametersInScenes;
+                blockdata.meta.numPropertiesInScenes = blockcopy.meta.numPropertiesInScenes;
+                blockdata.parameters = blockcopy.parameters;
+                blockdata.properties = blockcopy.properties;
+                blockdata.sceneValues = blockcopy.sceneValues;
+                blockdata.lastSavedSceneValues = blockcopy.lastSavedSceneValues;
+
                 if (!blockdata.enabled)
                 {
                     hostBypassBlockPair(hbp, true);
@@ -7013,14 +7025,23 @@ void HostConnector::initBlock(HostConnector::Block& blockdata,
                               const uint8_t numInputs,
                               const uint8_t numOutputs,
                               const uint8_t numSideInputs,
-                              const uint8_t numSideOutputs, 
-                              const Block* const blockDataToCopy) const
+                              const uint8_t numSideOutputs) const
 {
     assert(plugin != nullptr);
 
+    blockdata.enabled = true;
     blockdata.uri = plugin->uri;
+    blockdata.quickPotSymbol.clear();
     blockdata.plugin = plugin;
+
+    blockdata.meta.enable.hasScenes = false;
+    blockdata.meta.enable.hwbinding = UINT8_MAX;
+    blockdata.meta.enable.tempSceneState = kTemporarySceneNone;
+    blockdata.meta.enable.changesNotSavedToPreset = false;
     blockdata.meta.flags = plugin->flags;
+    blockdata.meta.quickPotIndex = 0;
+    blockdata.meta.numParametersInScenes = 0;
+    blockdata.meta.numPropertiesInScenes = 0;
     blockdata.meta.numInputs = numInputs;
     blockdata.meta.numOutputs = numOutputs;
     blockdata.meta.numSideInputs = numSideInputs;
@@ -7028,41 +7049,6 @@ void HostConnector::initBlock(HostConnector::Block& blockdata,
     blockdata.meta.name = plugin->name;
     blockdata.meta.abbreviation = plugin->abbreviation;
     blockdata.meta.category = plugin->category;
-
-    if (blockDataToCopy != nullptr)
-    {
-        const Block& blockcopy(*blockDataToCopy);
-        
-        blockdata.enabled = blockcopy.enabled;
-        blockdata.quickPotSymbol = blockcopy.quickPotSymbol;
-
-        blockdata.meta.enable = blockcopy.meta.enable;
-        blockdata.meta.quickPotIndex = blockcopy.meta.quickPotIndex;
-        blockdata.meta.numParametersInScenes = blockcopy.meta.numParametersInScenes;
-        blockdata.meta.numPropertiesInScenes = blockcopy.meta.numPropertiesInScenes;
-
-        blockdata.parameters = blockcopy.parameters;
-        blockdata.properties = blockcopy.properties;
-        blockdata.sceneValues = blockcopy.sceneValues;
-        blockdata.lastSavedSceneValues = blockcopy.lastSavedSceneValues;
-
-        // no need to do init of parameters, properties, scenes below
-        // as they were all just copied
-        return;
-    }
-    else
-    {
-        blockdata.enabled = true;
-        blockdata.quickPotSymbol.clear();
-
-        blockdata.meta.enable.hasScenes = false;
-        blockdata.meta.enable.hwbinding = UINT8_MAX;
-        blockdata.meta.enable.tempSceneState = kTemporarySceneNone;
-        blockdata.meta.enable.changesNotSavedToPreset = false;
-        blockdata.meta.quickPotIndex = 0;
-        blockdata.meta.numParametersInScenes = 0;
-        blockdata.meta.numPropertiesInScenes = 0;
-    }
 
     blockdata.parameterSymbolToIndexMap.clear();
     blockdata.propertyURIToIndexMap.clear();
